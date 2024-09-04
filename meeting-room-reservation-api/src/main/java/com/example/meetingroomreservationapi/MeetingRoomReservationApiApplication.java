7158facb924ebe7ce84c53aefc353e3e691d0ca8
Alirez-13 <alirezaholghi@gmail.com>
2024-09-03 17:23:51 -0700

    init java8 api

---
+++ meeting-room-reservation-api/src/main/java/com/example/meetingroomreservationapi/MeetingRoomReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetingroomreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomReservationApiApplication.class, args);
+	}
+
+}
---
+++ meeting-room-reservation-api/src/test/java/com/example/meetingroomreservationapi/MeetingRoomReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingroomreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
