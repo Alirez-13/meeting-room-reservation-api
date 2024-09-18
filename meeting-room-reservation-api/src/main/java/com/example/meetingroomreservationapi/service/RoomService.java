bf5e34dd2de75b4a8174f64ecfa813296738c0fa
narges82 <narges.nirmand@gmail.com>
2024-09-18 21:56:31 +0330

    add logger in service

--- meeting-room-reservation-api/src/main/java/com/example/meetingroomreservationapi/service/RoomService.java
+++ meeting-room-reservation-api/src/main/java/com/example/meetingroomreservationapi/service/RoomService.java
@@ -4,6 +4,8 @@ import com.example.meetingroomreservationapi.entity.Room;
 import com.example.meetingroomreservationapi.errHandler.NotFoundException;
 import com.example.meetingroomreservationapi.repository.RoomRepository;
 import com.example.meetingroomreservationapi.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,35 +15,46 @@ import java.util.stream.Collectors;
 
 @Service
 public class RoomService {
-
+    private static final Logger logger = LoggerFactory.getLogger(RoomService.class);
     @Autowired
     private RoomRepository roomRepository;
 
     public List<Room> getAllRooms() {
+        logger.debug("Fetching all rooms");
         return roomRepository.findAll();
     }
 
     public Room createRoom(Room room) {
+        logger.debug("Creating room: {}", room);
         return roomRepository.save(room);
     }
 
     public void deleteRoomById(Long id) {
+        logger.debug("Deleting room with ID: {}", id);
         Room room = roomRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Room with ID " + id + " not found."));
         roomRepository.deleteById(id);
+        logger.info("Room with ID {} deleted successfully", id);
     }
 
     public Room updateRoom(Long id, Room updatedRoom) {
+        logger.debug("Updating room with ID: {}", id);
         return roomRepository.findById(id)
                 .map(room -> {
                     room.setRoomCapacity(updatedRoom.getRoomCapacity());
                     room.setEmpty(updatedRoom.isEmpty());
+                    logger.info("Room with ID {} updated successfully", id);
                     return roomRepository.save(room);
                 })
-                .orElseThrow(() -> new NotFoundException("Room not found with id: " + id));
+                .orElseThrow(() -> {
+                    String errorMessage = "Room not found with id: " + id;
+                    logger.error(errorMessage);
+                    return new NotFoundException(errorMessage);
+                });
     }
 
     public List<Room> getEmptyRooms() { //available
+        logger.debug("Fetching empty rooms");
         return roomRepository.findByIsEmptyTrue();
     }
 }
--- meeting-room-reservation-api/src/main/java/com/example/meetingroomreservationapi/service/UserService.java
+++ meeting-room-reservation-api/src/main/java/com/example/meetingroomreservationapi/service/UserService.java
@@ -4,6 +4,8 @@ import com.example.meetingroomreservationapi.entity.User;
 import com.example.meetingroomreservationapi.errHandler.NotFoundException;
 import com.example.meetingroomreservationapi.errHandler.UnathorizeException;
 import com.example.meetingroomreservationapi.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,29 +17,40 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     public List<User> getAllUsers() {
         //   define a way to authenticate user role
+        logger.info("Fetching all users");
         List<User> usersList = userRepository.findAll();
         if (usersList.isEmpty()) {
+            logger.warn("User list is empty!");
             throw new NotFoundException("user list is empty !");
         }
         return usersList;
     }
 
     public User findUserById(Long userId) {
+        logger.info("Finding user with ID: {}", userId);
         return userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException("User not found with Id: " + userId));
+                .orElseThrow(() -> {
+                    logger.error("User not found with ID: {}", userId);
+                    return new NotFoundException("User not found with Id: " + userId);
+                });
     }
 
     public void saveUser(User user) {
         userRepository.save(user);
+        logger.info("User saved successfully with ID: {}", user.getId());
     }
 
     public void deleteUserById(Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException("User not found with id :"+userId ));
+                .orElseThrow(() -> {
+                    logger.error("User not found with ID: {}", userId);
+                    return new NotFoundException("User not found with id: " + userId);
+                });
         userRepository.deleteById(userId);
+        logger.info("User deleted successfully with ID: {}", userId);
     }
 
     public User updateUser(User updatedUser, Long id) {
@@ -45,8 +58,12 @@ public class UserService {
                 .map(user ->{
                     user.setPassword(updatedUser.getPassword());
                     user.setFullName(updatedUser.getFullName());
+                    logger.info("User updated successfully with ID: {}", id);
                     return userRepository.save(user);
                 })
-                .orElseThrow(() -> new NotFoundException("User not found with id :"+ id));
+                .orElseThrow(() -> {
+                    logger.error("User not found with ID: {}", id);
+                    return new NotFoundException("User not found with id: " + id);
+                });
     }
 }
